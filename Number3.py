5685719cf79287dc8080638590f2b9e345492689
Edward <62472600+horbosis556@users.noreply.github.com>
2020-10-12 14:12:46 +0700

    Update Number3.py

--- Number3.py
+++ Number3.py
@@ -1,7 +1,7 @@
 student_scores = [('Danny',91),('Sonny',93),('Naya',100),('Annabelle',89)]
 
-student_scores.sort(key = lambda x: x[1])
-#insert lambda to sort by second element as pivot in list.sort() method.
+student_scores.sort(reverse = true, key = lambda x: x[1])
+#insert lambda to sort descendingly by second element as pivot in list.sort() method.
 
 print(student_scores)
 
