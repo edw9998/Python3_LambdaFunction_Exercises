53f3f6dd6da685f1ea6e2a9d6764b2bc4fed882f
Edward <62472600+horbosis556@users.noreply.github.com>
2020-10-12 12:43:25 +0700

    Update Number1.py

--- Number1.py
+++ Number1.py
@@ -1,5 +1,5 @@
-f = lambda x: x + 15
+f = lambda x: x + 15    #This lambda function adds 15 to an integer argument.
 print(f(15))
 
-v = lambda y, z: y * z
+v = lambda y, z: y * z  #This other lambda function multiplies two integer arguments.
 print(v(15, 15))
