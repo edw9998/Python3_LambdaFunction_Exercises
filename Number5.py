9b563190eaf3c95a9e527cb5b3f8190e43478aa7
Edward <62472600+horbosis556@users.noreply.github.com>
2020-10-12 13:58:09 +0700

    Add files via upload

---
+++ Number5.py
@@ -0,0 +1,15 @@
+def fibonacci(n):
+    #Fn = Fn-1 + Fn-2  with seed values F0 = 0 and F1 = 1.
+    #Initialize 0, 1 as first and second term.
+    start = [0, 1]
+
+    #Map every fibonacci terms unto nth term using formula Fn = Fn-1 + Fn-2.
+    list(map(lambda x: start.append(sum(start[-2:])),range(2, n)))
+
+    #Stop appending fibonacci elements when the nth term has been reached.
+    return start[:n]
+
+v = int(input("Enter Fibonacci's nth term: "))
+print(fibonacci(v))
+
+
