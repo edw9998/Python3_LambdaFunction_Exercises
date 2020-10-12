81b15889618c0401232480266188d97c481f3ad6
Edward <62472600+horbosis556@users.noreply.github.com>
2020-10-12 13:25:00 +0700

    Add files via upload

---
+++ Number3.py
@@ -0,0 +1,14 @@
+student_scores = [('Danny',91),('Sonny',93),('Naya',100),('Annabelle',89)]
+
+student_scores.sort(key = lambda x: x[1])
+#insert lambda to sort by second element as pivot in list.sort() method.
+
+print(student_scores)
+
+
+
+
+
+
+
+
