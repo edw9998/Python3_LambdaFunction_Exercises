cb636eb4796c725acfd037242b6c63d54ab4ff47
Edward <62472600+horbosis556@users.noreply.github.com>
2020-10-12 12:58:53 +0700

    Update Number2.py

--- Number2.py
+++ Number2.py
@@ -1,12 +1,11 @@
 from random import randint
 
 def random_multiplication(n):
-    global m
-    m = randint(1, 100)
-    return n * m
+    m = randint(1, 100)         # Multiply int(input()) with a random integer between 1 and 100 --> Both are included.                  
+    return n * m                
 
-x = int(input("Enter Multiplicand for random integer: "))
-print("Result =", random_multiplication(x))
+x = int(input("Enter Multiplicand for random integer: "))   # Enter a multiplicand to be multiplied with the generated randint.    
+print("Result =", random_multiplication(x))                 # Return multiplication result.
 
 
 
